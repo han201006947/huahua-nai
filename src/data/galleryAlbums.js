611db21fb3bbe3658b3f59eb59fea0d6ae00c56a
han <51391334+han201006947@users.noreply.github.com>
2026-06-09 19:29:20 +0800

    chore: sync galleryAlbums from mobile admin

--- src/data/galleryAlbums.js
+++ src/data/galleryAlbums.js
@@ -247,4 +247,18 @@ export const galleryAlbums = [
       { type: 'video', src: './zhuti/z2-jiehun.mp4' },
     ],
   },
+  {
+    id: 'zhuti-z3',
+    title: '主题款 z3',
+    category: '主题款',
+    cover: 'blob:https://han201006947.github.io/1df7e057-1c42-45f5-b916-f2354c7fa4f0',
+    hasVideo: false,
+    stylePreview: true,
+    media: [
+      { type: 'image', src: 'blob:https://han201006947.github.io/1df7e057-1c42-45f5-b916-f2354c7fa4f0' },
+      { type: 'image', src: 'blob:https://han201006947.github.io/af8db7ca-889e-403e-b6eb-738fae9fdc9b' },
+      { type: 'image', src: 'blob:https://han201006947.github.io/cd08c02d-d031-4518-9647-080d1e94a023' },
+      { type: 'image', src: 'blob:https://han201006947.github.io/f6c2504d-e9b2-442e-965d-87dfda9a6c02' },
+    ],
+  },
 ]
